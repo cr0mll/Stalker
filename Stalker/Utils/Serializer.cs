d8035b59f2ccc542a5cf6ff586fbd4b99089387b
cr0mll <cr0mll@protonmail.com>
2023-12-28 23:45:02 +0200

    Deserialize from XML file

--- Stalker/Utils/Serializer.cs
+++ Stalker/Utils/Serializer.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Runtime.Serialization;
 using System.Xml.Serialization;
 
 namespace Stalker.Utils;
@@ -6,7 +7,7 @@ namespace Stalker.Utils;
 public static class Serializer
 {
     /// <summary>
-    /// A method to serialize an object to an XML file.
+    /// Serialize an object to an XML file.
     /// </summary>
     /// <param name="objectToSerialize">The object to serialize.</param>
     /// <param name="filepath">The filepath where the XML file should be saved.</param>
@@ -16,5 +17,18 @@ public static class Serializer
         var xmlSerializer = new XmlSerializer(typeof(T));
         using var writer = new StreamWriter(filepath);
         xmlSerializer.Serialize(writer, objectToSerialize);
+    }
+
+    /// <summary>
+    /// Deserialises an XML file into the an object of the specified type.
+    /// </summary>
+    /// <param name="filepath">The path of the XML file to deserialise.</param>
+    /// <typeparam name="T">The type of object to be created.</typeparam>
+    /// <returns>An object deserialised from the XML file.</returns>
+    public static T DeserializeXmlFile<T>(string filepath)
+    {
+        var xmlSerializer = new XmlSerializer(typeof(T));
+        using var reader = new StreamReader(filepath);
+        return (T)xmlSerializer.Deserialize(reader) ?? throw new SerializationException();
     }
 }
--- Tests/Utils/SerializerTest.cs
+++ Tests/Utils/SerializerTest.cs
@@ -20,4 +20,16 @@ public class SerializerTest
         Serializer.SerializeToXmlFile(inv, Path.GetFullPath("../../../SandboxInvestigation/Investigation.xml"));
         Assert.Pass();
     }
+
+    [Test]
+    public void TestDeserializeXmlFile()
+    {
+        Investigation inv = new Investigation();
+        
+        Serializer.SerializeToXmlFile(inv, Path.GetFullPath("../../../SandboxInvestigation/Investigation.xml"));
+        Investigation inv2 = Serializer.DeserializeXmlFile<Investigation>(
+            Path.GetFullPath("../../../SandboxInvestigation/Investigation.xml"));
+        
+        Assert.Pass(); // Verified via debugger 
+    }
 }
